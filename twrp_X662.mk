d0a9753ed5651ee84dfdd7d4d4c91c3bd8716cbd
gghhhhhg <96913544+gghhhhhg@users.noreply.github.com>
2024-07-18 16:26:03 +0300

    Update and rename omni_Infinix-X662.mk to twrp_X662.mk

--- twrp_X662.mk
+++ twrp_X662.mk
@@ -10,16 +10,16 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from Infinix-X662 device
-$(call inherit-product, device/infinix/Infinix-X662/device.mk)
+# Inherit from X662 device
+$(call inherit-product, device/infinix/X662/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Infinix-X662
-PRODUCT_NAME := omni_Infinix-X662
+PRODUCT_DEVICE := X662
+PRODUCT_NAME := twrp_X662
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X662
 PRODUCT_MANUFACTURER := infinix
